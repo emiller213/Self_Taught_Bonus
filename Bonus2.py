1217bb7bd6bbaf45e3db35e445e45a08574e3280
emiller213 <emiller213@gmail.com>
2019-04-22 21:57:39 -0400

    Refactor

--- Bonus2.py
+++ Bonus2.py
@@ -3,26 +3,20 @@ import random
 
 def quiz():
     correct_answers = 0
-    incorrect_answers = 0
-    quiz_questions = open("questions.txt", "r")
-    quiz_questions_list = quiz_questions.readlines()
-    random.shuffle(quiz_questions_list)
+    with open("questions.txt") as file:
+        quiz_questions_list = file.readlines()
+        random.shuffle(quiz_questions_list)
     for i in range(0, 5):
-        question = quiz_questions_list[i].strip()
-        question_answer = question.split(",")
-        question_split = question_answer[0]
-        answer_split = question_answer[1].lower()
-        user_response = input(question_split + ' ').lower()
-        if user_response == answer_split:
+        question_answer = quiz_questions_list[i].strip().split(",")
+        user_response = input(question_answer[0] + ' ').lower()
+        if user_response == question_answer[1].lower():
             print("Correct!")
             correct_answers += 1
         else:
             print("Incorrect!")
-            print("The correct answer is: " + answer_split.capitalize() + ".")
-            incorrect_answers += 1
-    score = 20 * correct_answers
+            print("The correct answer is: " + question_answer[1].capitalize() + '.')
     print("Quiz complete!  You answered " + str(correct_answers) + " correctly!")
-    print("You scored " + str(score) + "%")
+    print("You scored " + str(correct_answers * 20) + "%")
 
 
 quiz()
