8fa479eedb7c6a9dab20d108c76351787be49447
emiller213 <emiller213@gmail.com>
2019-05-04 17:43:50 -0400

    initial commit

--- Bonus4.py
+++ Bonus4.py
@@ -1,7 +1,17 @@
 import csv
+import pandas as pd
+# from collections import OrderedDict
+# import operator
 
 filename = 'Countries.csv'
-with open(filename) as f:
-    reader = csv.reader(f)
-    header_row = next(reader)
-    print(header_row)
+choice = input("How would you like to sort? :")
+df1 = pd.read_csv(filename)
+df2 = df1.sort_values(by=[choice], ascending=True)
+
+df2.to_csv('my_new_csv.csv')
+
+
+with open('my_new_csv.csv') as csvfile:
+    readfile = csv.reader(csvfile)
+    for row in readfile:
+        print(row)
