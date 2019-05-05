c66339cad7add7fe5cf49520c1fdeb736f0717d0
emiller213 <emiller213@gmail.com>
2019-05-05 10:02:50 -0400

    added line 10 to write to new csv file

--- Bonus4.py
+++ Bonus4.py
@@ -5,11 +5,10 @@ import pandas as pd
 
 filename = 'Countries.csv'
 choice = input("How would you like to sort? :")
-df1 = pd.read_csv(filename)
+df1 = pd.read_csv(filename, header=0)
 df2 = df1.sort_values(by=[choice], ascending=True)
-
 df2.to_csv('my_new_csv.csv')
-
+print(df2)
 
 with open('my_new_csv.csv') as csvfile:
     readfile = csv.reader(csvfile)
