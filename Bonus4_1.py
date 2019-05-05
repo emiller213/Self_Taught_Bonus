7b4140fd8256d56567eb79fbbd47d47c723b8992
emiller213 <emiller213@gmail.com>
2019-05-05 11:47:36 -0400

    initial commit

---
+++ Bonus4_1.py
@@ -0,0 +1,24 @@
+import csv
+
+
+user_input = input("What would you like to sort by? (Country,Name,GDPPC, Literacy,InfantMortality, Agriculture,	Population,	NetMigration)")
+
+
+
+with open('Countries.csv', 'r', newline='') as input_file:
+    csv_input = csv.DictReader(input_file)
+    data = sorted(csv_input,reverse=True, key=lambda row: (row[user_input]))
+
+with open('Countries_Sorted.csv', 'w') as output_file:
+    csv_output = csv.DictWriter(output_file, fieldnames=csv_input.fieldnames)
+    csv_output.writeheader()
+    csv_output.writerows(data)
+
+
+with open('Countries_Sorted.csv', 'r') as final_file:
+    for line in final_file:
+        print(line)
+
+
+
+
