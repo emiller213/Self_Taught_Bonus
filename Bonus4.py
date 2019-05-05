1c34a5fa8ea088b4e42d116671d30c98f0f9a34b
emiller213 <emiller213@gmail.com>
2019-05-05 11:48:36 -0400

    rewrote entire program not using pandas and only using the csv module

--- Bonus4.py
+++ Bonus4.py
@@ -1,16 +1,20 @@
 import csv
-import pandas as pd
-# from collections import OrderedDict
-# import operator
 
-filename = 'Countries.csv'
-choice = input("How would you like to sort? :")
-df1 = pd.read_csv(filename, header=0)
-df2 = df1.sort_values(by=[choice], ascending=True)
-df2.to_csv('my_new_csv.csv')
-print(df2)
 
-with open('my_new_csv.csv') as csvfile:
-    readfile = csv.reader(csvfile)
-    for row in readfile:
-        print(row)
+user_input = input("What would you like to sort by? (Country,Name,GDPPC, Literacy,InfantMortality, Agriculture,	Population,	NetMigration)")
+
+
+
+with open('Countries.csv', 'r', newline='') as input_file:
+    csv_input = csv.DictReader(input_file)
+    data = sorted(csv_input,reverse=True, key=lambda row: (row[user_input]))
+
+with open('Countries_Sorted.csv', 'w') as output_file:
+    csv_output = csv.DictWriter(output_file, fieldnames=csv_input.fieldnames)
+    csv_output.writeheader()
+    csv_output.writerows(data)
+
+
+with open('Countries_Sorted.csv', 'r') as final_file:
+    for line in final_file:
+        print(line)
