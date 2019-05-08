c304dbf60e864977edd540c0bd98b3c91b239da2
emiller213 <emiller213@gmail.com>
2019-05-08 15:38:48 -0400

    commit

--- example_csv_file.py
+++ example_csv_file.py
@@ -5,16 +5,12 @@ user_input = input(
 
 with open('Countries.csv', 'r', newline='') as input_file:
     csv_input = csv.DictReader(input_file)
-    print(csv_input)
     f1 = lambda row: row[user_input]
     f2 = lambda row: int(row[user_input])
-    f3 = lambda row: float(row[user_input])
-    if user_input == 'Country' or 'GDPPC' or 'Population':
+    if user_input == 'Country':
         data = sorted(csv_input, reverse=True, key=f2)
-    elif user_input == 'Literacy' or 'InfantMortality' or 'Agriculture' or 'NetMigration':
-        data = sorted(csv_input, reverse=True, key=f3)
     else:
-        data = sorted(csv_input, reverse=False, key=f1)
+        data = sorted(csv_input, reverse=True, key=f1)
 
 with open('Countries_Sorted.csv', 'w') as output_file:
     csv_output = csv.DictWriter(output_file, fieldnames=csv_input.fieldnames)
@@ -24,4 +20,3 @@ with open('Countries_Sorted.csv', 'w') as output_file:
 with open('Countries_Sorted.csv', 'r') as final_file:
     for line in final_file:
         print(line)
-
