eb8aa99ea48ecace8393395632f6dd82c397aa17
emiller213 <emiller213@gmail.com>
2019-05-09 19:11:29 -0400

    initial commit

--- Bonus5.py
+++ Bonus5.py
@@ -1,25 +1,34 @@
 class BankAccount:
-    balance = float(500.15)
+    balance = 150.346
 
 
 class Deposit:
     def __init__(self, deposit_amount):
         self.deposit_amount = deposit_amount
         BankAccount.balance += float(deposit_amount)
-        print("Your account balance is $" + repr(BankAccount.balance))
+        print("Your account balance is ${:.2f}".format(BankAccount.balance))
+
 
 class Withdrawal:
     def __init__(self, withdrawal_amount):
         self.withdrawal_amount = withdrawal_amount
         BankAccount.balance -= float(withdrawal_amount)
-        print("Your account balance is $" + repr(BankAccount.balance))
-
+        print("Your account balance is ${:.2f}".format(BankAccount.balance))
 
 
 print("Welcome to Bank of Python")
-print("You account balance is $" + repr(BankAccount.balance))
+print("Press 1 to see your account balance.")
+print("Press 2 to make a deposit.")
+print("Press 3 to make a withdrawal.")
+action = int(input("Please make your selection now: "))
 
-Deposit(100)
-Withdrawal(50.15)
-
-
+if action == 1:
+    print("Your account balance is ${:.2f}".format(BankAccount.balance))
+elif action == 2:
+    money_amount = input("How much would you like to deposit? :")
+    Deposit(money_amount)
+elif action == 3:
+    money_amount = input("How much would like like to withdrawal? :")
+    Withdrawal(money_amount)
+else:
+    print("Invalid Entry.  Goodbye :)")
